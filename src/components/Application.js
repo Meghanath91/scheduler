b8743d25b17ce45a3ea87054eece7c49bae24b0c
Meghanath91 <meghanath.balaji@gmail.com>
2020-02-19 20:26:33 -0500

    dayslist hydrated with API axios

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,31 +1,30 @@
 // import React from "react";
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import Appointment from "components/Appointment";
+import axios from "axios"
+// const axios = require('axios')
 
-import Header from "components/Appointment/Header";
-import Show from "components/Appointment/Show";
-import Empty from "components/Appointment/Empty";
 
 import "components/Application.scss";
 import DayList from "components/ DayList";
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0
-  }
-];
+// const days = [
+//   {
+//     id: 1,
+//     name: "Monday",
+//     spots: 2,
+//   },
+//   {
+//     id: 2,
+//     name: "Tuesday",
+//     spots: 5,
+//   },
+//   {
+//     id: 3,
+//     name: "Wednesday",
+//     spots: 0,
+//   },
+// ];
 
 const appointments = [
   {
@@ -75,7 +74,24 @@ const appointments = [
 ];
 
 export default function Application() {
-  const [day, setDay] = useState("Monday");
+
+  const [day, setDay] = useState("");
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:8001/api/days").then((response) => {
+      
+      setDay("Monday")
+      setDays(response.data);
+
+    });
+
+  }, [])
+
+
+
+  
+ 
 
   return (
     <main className="layout">
@@ -87,7 +103,11 @@ export default function Application() {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList days={days} day={day} setDay={setDay} />
+          <DayList 
+            days={days} 
+            day={day} 
+            setDay={setDay} 
+            />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
