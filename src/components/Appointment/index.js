6715147a19c2ac0433b95d5e11087d2a9494add1
Meghanath91 <meghanath.balaji@gmail.com>
2020-02-21 14:49:49 -0500

    Cancel button hydrated

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,9 +5,10 @@ import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
 import useVisualMode from "../../hooks/useVisualMode"
-
+import Form from "components/Appointment/Form"
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
+const CREATE= "CREATE";
 
 
 
@@ -19,13 +20,19 @@ export default function Appointment(props) {
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {mode === EMPTY && <Empty onAdd= {props.onAdd} />}
+      {mode === EMPTY && <Empty onAdd= {()=> transition(CREATE)} />}
       {mode === SHOW && (
       <Show
       student={props.interview.student}
       interviewer={props.interview.interviewer}
       />
       )}
+      {
+        mode === CREATE && <Form 
+        onCancel = {()=> back(EMPTY)}
+        interviewer = {[]}
+        />
+      }
     </article>
   );
 }
