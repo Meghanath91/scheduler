b4f122e23575bba6ee89378dd6eec8ad3dfbe7cd
Meghanath91 <meghanath.balaji@gmail.com>
2020-04-14 13:38:29 -0400

    netlify.toml added and axios configured in index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 // ROOT APPLICATION LAYER
 import React from "react";
 import ReactDOM from "react-dom";
+import axios from "axios";
 
 import "index.scss";
 
 import Application from "components/Application";
+
+// to configure the default base URL for production
+if (process.env.REACT_APP_API_BASE_URL) {
+  axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+}
 //Rendering  Application to React Server
 ReactDOM.render(<Application />, document.getElementById("root"));
 
